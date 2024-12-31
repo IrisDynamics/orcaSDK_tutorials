00ffc0c5958e502467116d48b445cb0e70fdd9ec
Aiden Bull <aidenbull@hotmail.com>
2024-12-31 15:05:47 -0800

    Removing logging from command stream tutorial. It doesn't seem necessary

--- Tutorials/06_CommandStream/main.cpp
+++ Tutorials/06_CommandStream/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "actuator.h"
 #include "TutorialHelpers.h"
-#include "tools/log.h"
 
 int main() {
 	Actuator motor{ "MyMotorName" };
@@ -12,22 +11,11 @@ int main() {
 
 	motor.enable_stream(); //Begin streaming
 
-	Log log{ Log::TimestampType::DurationSinceOpening };
-	OrcaError error = log.open("tutorial_log");
-	if (error)
-	{
-		std::cout << error.what();
-		return -1;
-	}
-
 	while (true) {
 		motor.run(); // Adds a stream command if there isn't already outgoing data
 
-		int32_t current_position = motor.stream_cache.position; //Using the stream cache, not a read
-		std::string curr_position_str = "Current Position: " + std::to_string(current_position);
-
-		log.write(curr_position_str);
-		std::cout << curr_position_str << "                    \r";
+		//Note: Using motor.stream_cache.position rather than motor.get_position_um()
+		std::cout << "Current Position: " << motor.stream_cache.position << "             \r";
 	}
 
 	return 0;
