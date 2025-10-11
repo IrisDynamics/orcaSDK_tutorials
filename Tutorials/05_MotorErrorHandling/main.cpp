04623bececb75b2958da152bdb52367a1962aba4
Aiden Bull <aidenbull@hotmail.com>
2025-10-10 17:34:40 -0700

    Added new tutorial for handling internal motor errors

---
+++ Tutorials/05_MotorErrorHandling/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "actuator.h"
+#include "TutorialHelpers.h"
+
+using namespace orcaSDK;
+
+int main() {
+	Actuator motor{ "MyMotorName" };
+
+	int serial_port = obtain_serial_port_number();
+
+	motor.open_serial_port(serial_port);
+
+	OrcaResult<uint16_t> motor_errors_result = motor.get_errors();
+
+	if (motor_errors_result.error)
+	{
+		std::cout << "Failed to read active errors: " << motor_errors_result.error.what() << "\n";
+		return 1;
+	}
+
+	std::cout << "Active motor errors: " << motor_errors_result.value << "\n";
+
+	if (motor_errors_result.value & 1024) std::cout << "There is definitely an invalid supply voltage error!" << "\n";
+
+	return 0;
+}
